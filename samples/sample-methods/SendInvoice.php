e910328470df24a5e145ed398b20a37d8a0ff824
meet-creedally <meet.mehta@creedally.com>
2025-11-25 19:06:18 +0530

    Conversion of SendInvoice Completed

---
+++ samples/sample-methods/SendInvoice.php
@@ -0,0 +1,41 @@
+<?php
+// Include required library files.
+require_once('../../includes/config.php');
+require_once('../../autoload.php');
+
+// Create PayPal object.
+$PayPalConfig = array(
+	'Sandbox' => $sandbox,
+	'DeveloperAccountEmail' => $developer_account_email,
+	'ApplicationID' => $application_id,
+	'DeviceID' => $device_id,
+	'IPAddress' => $_SERVER['REMOTE_ADDR'],
+	'PayPalAPIMode' => $api_mode,
+	'APIUsername' => $api_username,
+	'APIPassword' => $api_password,
+	'APISignature' => $api_signature,
+	'APISubject' => $api_subject,
+	'PrintHeaders' => $print_headers, 
+	'LogResults' => $log_results, 
+	'LogPath' => $log_path,
+	'isAdaptive' => true,
+	'PayPalAPIUpgrade' => $api_upgrade,
+	'ClientID' => $rest_client_id,
+	'ClientSecret' => $rest_client_secret,
+);
+
+$PayPal = angelleye\PayPal\PayPal::init($PayPalConfig);
+
+// Pass data into class for processing with PayPal and load the response array into $PayPalResult
+$InvoiceID = 'INV2-G6YX-H4CK-6LUE-6JXP';
+
+if( $api_mode === 'rest' ){
+	$PayPalResult = $PayPal->SendInvoice($InvoiceID);
+	echo '<pre />';
+} else {
+	$PayPalResult = $PayPal->Adaptive->SendInvoice($InvoiceID);
+	echo '<pre />';
+    echo "<p><strong>Deprecated Notice:</strong> The classic CreateRecurringPaymentsProfile method your plugin/theme is using has been deprecated. Please upgrade to the new REST-based implementation to ensure compatibility with future updates.</p>";
+}
+
+print_r($PayPalResult);
