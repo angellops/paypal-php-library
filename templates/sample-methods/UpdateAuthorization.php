d7e3a6d17f38031dd30a45d30bf8ba737ac8a01e
meet-creedally <meet.mehta@creedally.com>
2025-12-01 12:29:44 +0530

    Templates Samples Added

---
+++ templates/sample-methods/UpdateAuthorization.php
@@ -0,0 +1,46 @@
+<?php
+// Include required library files.
+require_once('../../includes/config.php');
+require_once('../../autoload.php');
+
+// Create PayPal object.
+$PayPalConfig = array(
+	'Sandbox' => $sandbox,
+	'PayPalAPIMode' => $api_mode,
+	'APIUsername' => $api_username,
+	'APIPassword' => $api_password,
+	'APISignature' => $api_signature, 
+	'PrintHeaders' => $print_headers,
+	'LogResults' => $log_results,
+	'LogPath' => $log_path,
+	'PayPalAPIUpgrade' => $api_upgrade,
+    'ClientID' => $rest_client_id,
+    'ClientSecret' => $rest_client_secret
+);
+
+$PayPal = angelleye\PayPal\PayPal::init($PayPalConfig);
+
+if ( $api_mode === 'rest' ) {
+
+	$transactionID = '';
+
+	// Pass data into class for processing with PayPal and load the response array into $PayPalResult
+	$PayPalResult = $PayPal->UpdateAuthorization($transactionID);
+
+} else {
+
+	// Prepare request arrays
+	$UAFields = array(
+		'transactionid' => '', 			// Required. The value of the authorization's transaction identification number returned by a PayPal product.  Char length: 17 single-byte chars.
+		'ipaddress' => ''			// IP address of the customer.
+	);
+					
+	$PayPalRequestData = array('UAFields'=>$UAFields);
+
+	// Pass data into class for processing with PayPal and load the response array into $PayPalResult
+	$PayPalResult = $PayPal->UpdateAuthorization($PayPalRequestData);
+}
+
+// Write the contents of the response array to the screen for demo purposes.
+echo '<pre />';
+print_r($PayPalResult);
